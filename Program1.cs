46ffab278aff8038b9b226784c9e18fc5cda3bc6
bala8729 <102343734+bala8729@users.noreply.github.com>
2022-04-27 11:41:15 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StudentInformation
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            TeacherStudentInfo tsi1 = new TeacherStudentInfo(11, "Tiwari", "Bala","OA3");
+            TeacherStudentInfo tsi2 = new TeacherStudentInfo(12, "Kishore", "Bala", "IMOD");
+            TeacherStudentInfo tsi3 = new TeacherStudentInfo(13, "Bharath", "Bala", "DT");
+            TeacherStudentInfo tsi4 = new TeacherStudentInfo(14, "Radhika", "Bala", "Federal");
+            Console.WriteLine("Student ID Student Name Student Teacher Student Subject");
+            tsi1.GetStudentDetails();
+            tsi2.GetStudentDetails();
+            tsi3.GetStudentDetails();
+            tsi4.GetStudentDetails();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
---
+++ TeacherStudentInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformation
+{
+    class TeacherStudentInfo:School
+    {
+        public TeacherStudentInfo(int id, string sname, string steacher, string subject) : base(id, sname, steacher, subject)
+        {
+            
+        }
+    }
+}
